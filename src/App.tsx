4184b00caecd1eb36eb0b5b4fc6764257afc09cd
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-07-05 12:09:14 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,21 @@ import { GlobalStyle } from './style/Main.style'
 
 const App = () => {
 
-  const [totalQuestions, setTotalQuestions] = useState(0)
-  const [category, setCategory] = useState(0)
-  const [difficulty, setdifficulty] = useState(Difficulty.EASY)
+  // const [totalQuestions, setTotalQuestions] = useState(0)
+  // const [category, setCategory] = useState(0)
+  // const [difficulty, setdifficulty] = useState(Difficulty.EASY)
+
+  const [quiz, setQuiz] = useState(
+    {totalQuestions : 0,
+    category: 0,
+    difficulty: Difficulty.EASY});
 
   return (
     <>
       <GlobalStyle />
       <BrowserRouter>
-        <Route path="/" exact component={() => <Home setTotalQuestions={setTotalQuestions} setCategory={setCategory} setdifficulty={setdifficulty} />} />
-        <Route path="/play" exact component={() => <MainP totalQuestions={totalQuestions} category={category} difficulty={difficulty} />} />
+        <Route path="/" exact component={() => <Home setQuiz={setQuiz}/>} />
+        <Route path="/play" exact component={() => <MainP quiz = {quiz} />} />
       </BrowserRouter>
     </>
   )
