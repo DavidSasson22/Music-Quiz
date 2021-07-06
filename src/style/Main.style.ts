1fa802a2365b02929497479f26f8fc32559db02a
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-07-06 09:43:38 +0300

    Update Main.style.ts

--- src/style/Main.style.ts
+++ src/style/Main.style.ts
@@ -1,52 +1,44 @@
-import styled from "styled-components";
-import background from '../assets/img/main-background.jpg'
+import styled, { createGlobalStyle } from "styled-components";
 
+export const GlobalStyle = createGlobalStyle`
+  *, html, body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Times New Roman';    
+  }
 
-export const HomePage = styled.div`
+ 
+  h1 {
+    text-transform: uppercase;
+    line-height: 2;
+  }
+`;
+
+export const Main = styled.div`
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
-  text-align: center;
-  background-image: url(${background});
-  background-position: center center;
-     background-repeat: no-repeat; 
-     -webkit-background-size: cover;
-     -moz-background-size: cover;
-     -o-background-size: cover;
-     background-size: cover;
-  > header {
-    padding: 16px;
-    margin-bottom: 32px;
-    max-width: 84vw;
+  flex-direction: column;
+  > .score {
+    font-size: 2rem;
+    line-height: 2;
   }
+  .start, .next {
+    width: 200px;
+    height: 60px;
+    border-radius: 60px;
+    font-size: large;
+    background-color: #293540;
+    border: 0px;
+    box-shadow: 0;
+    color: white;
+    margin: 16px;
 
-  > form {
-    background-color: rgb(226,231,235);
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    height: 400px;
-    padding: 16px;
+    &:hover {
+      cursor: pointer;
+    }
   }
-    .field {
-      display: flex;
-      flex-direction: column;
-      height: 15%;
-      justify-content: space-between;
-      > select, input, label {
-        padding: 4px;
-      }
-    }
-    button {
-      padding: 8px;
-      font-weight: 700;
-      background-color: black;
-      color: white;
-      &:hover {
-        cursor: pointer;
-      }
-    }
-`
+`;
