41bacb2da07310225f63ce29a7dad88682f748ba
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-07-06 09:41:42 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,6 @@ import { GlobalStyle } from './style/Main.style'
 
 const App = () => {
 
-  // const [totalQuestions, setTotalQuestions] = useState(0)
-  // const [category, setCategory] = useState(0)
-  // const [difficulty, setdifficulty] = useState(Difficulty.EASY)
 
   const [quiz, setQuiz] = useState(
     {totalQuestions : 0,
