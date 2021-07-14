e9c04fe140c3de78df0624dfb94c334e04ed14e8
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-07-14 17:41:31 +0300

    style bug fixed

--- src/style/Main.style.ts
+++ src/style/Main.style.ts
@@ -17,7 +17,7 @@ export const GlobalStyle = createGlobalStyle`
 
 export const Main = styled.div`
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
